b7575ec25e8b1251e06d176f80ab1d3f03e9ad29
Elliott <elliott@mtx.studio>
2025-01-30 17:52:42 +0100

    formatting

--- src/Token.sol
+++ src/Token.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import { ERC20Burnable } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import { ERC20Capped } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import { AccessControlDefaultAdminRules } from "@openzeppelin/contracts/access/AccessControlDefaultAdminRules.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {ERC20Capped} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {AccessControlDefaultAdminRules} from "@openzeppelin/contracts/access/AccessControlDefaultAdminRules.sol";
 
 contract Token is ERC20, ERC20Burnable, AccessControlDefaultAdminRules {
     bytes32 public constant TRANSFER_ROLE = keccak256("TRANSFER_ROLE");
@@ -13,8 +13,10 @@ contract Token is ERC20, ERC20Burnable, AccessControlDefaultAdminRules {
 
     bool public transfersEnabled = false;
 
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) AccessControlDefaultAdminRules(0, msg.sender) {
-    }
+    constructor(string memory name, string memory symbol)
+        ERC20(name, symbol)
+        AccessControlDefaultAdminRules(0, msg.sender)
+    {}
 
     function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
         _mint(to, amount);
@@ -30,7 +32,9 @@ contract Token is ERC20, ERC20Burnable, AccessControlDefaultAdminRules {
         // Do the following check if transfers are not enabled yet
         if (!transfersEnabled) {
             // from address has to be either the zero address (mint event), the owner or someone with TRANSFER_ROLE
-            require(from == address(0) || from == owner() || hasRole(TRANSFER_ROLE, from), "ERC20: transfers not enabled");
+            require(
+                from == address(0) || from == owner() || hasRole(TRANSFER_ROLE, from), "ERC20: transfers not enabled"
+            );
         }
     }
 }
